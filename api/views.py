bf1d2fb0327d10ded46305931f59b83706e7a3cb
chris2009 <christian.1582@gmail.com>
2024-03-14 23:12:58 -0500

    Actualizacion de Pipeline para Despliegue - SECRETS para produccion

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
